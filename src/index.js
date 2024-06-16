2a7c0ad52c61de13dd21a60aa5cfdbe835272929
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-06-16 20:57:48 -0300

    commit_ conect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
+mongoose.connect('mongodb+srv://viniciusjosepereira:UCxycxr0KzmF2BWu@fut7-api.nwgbren.mongodb.net/?retryWrites=true&w=majority&appName=Fut7-API')
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
