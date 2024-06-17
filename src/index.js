ad8c1d9b50ea8d151a4f56c994676232f57a08b7
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-06-16 21:27:13 -0300

    commit_ Delete

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 mongoose.connect('mongodb+srv://viniciusjosepereira:UCxycxr0KzmF2BWu@fut7-api.nwgbren.mongodb.net/?retryWrites=true&w=majority&appName=Fut7-API')
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+const Player = mongoose.model('Player', {
+        number: Number,
+        name: String,
+        goals: Number,
+        assists: Number
+});
+
+app.get("/", async (req, res) => {
+    const players = await Player.find();
+    return res.send(players);
+})
+
+app.delete("/:id", async (req, res) => {
+    const player = await Player.findById(req.params.id);
+    return res.send(player);
+})
+
+app.post("/", async (req, res) => {
+    const player = new Player({
+        number: req.body.number,
+        name: req.body.name,
+        goals: req.body.goals,
+        assists: req.body.assists
+    })
+
+    await player.save();
+    return res.send(player);
 })
 
 app.listen(port, () => {
